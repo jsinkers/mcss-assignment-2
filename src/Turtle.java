5f6223e84c986a5ae9ed82f740a66d2b405bacb2
Orange Man <303221459@qq.com>
2021-05-19 02:21:58 +1000

    update grainAhead method
    Updated. need to double check how to use the method "World.getInstance().getHeadingPatches()" in the world class

--- src/Turtle.java
+++ src/Turtle.java
@@ -1,13 +1,21 @@
+import java.util.List;
+
 /**
  * Represents a turtle able to move through the world, harvesting grain
  */
 public class Turtle {
     private int age;
     private int wealth;
+    private int x;  //the current turtle position in x-axis
+private int y;   //the current turtle position in y-axis
+
 
     private final int lifeExpectancy;
     private final int metabolism;
     private final int vision;
+    //direction of where the turtle is heading (degree)
+    //TODO: check if we still decide to use degree to represent the direction
+    private int heading;
 
     public Turtle(int lifeExpectancy, int metabolism, int vision) {
         this.lifeExpectancy = lifeExpectancy;
@@ -16,14 +24,13 @@ public class Turtle {
     }
 
 
-    /*
-    determine the direction which is most profitable for each turtle in
-    the surrounding patches within the turtles' vision
-
-    TODO: need to sync the heading patch mechanism with "World" class
+    /**
+     * determine the direction which is most profitable for each turtle in
+     * the surrounding patches within the turtles' vision
+     * TODO: need to sync the heading patch mechanism with "World" class
      */
     private void turnTowardsGrain() {
-        int heading = 0;
+         heading = 0;
         int bestDirection = 0;
         int bestAmount = grainAhead();
         //try another direction (in this case 90 degree) to
@@ -48,8 +55,26 @@ public class Turtle {
         heading=bestDirection;
     }
 
-    private void grainAhead() {
-
+    /**
+     * calculate the total number of grain in the heading patches that can be seen by the turtle
+     *
+     * @return the total number of grain in the heading patches that can be seen by the turtle
+     */
+    private int grainAhead() {
+        //the total grain in the heading patches that can be seen by the turtle
+        int total = 0;
+        //how far the patch is ahead of a turtle, the initial heading patch is 1 distance ahead
+        int howFar= 1;
+        //TODO: need to double check if this method is used correctly
+        //get a list of patches which can be seen by the turtle in its heading direction
+        //the number of the patches in the list depends on the vision of the turtle
+        List<Patch> headingPatches = World.getInstance().getHeadingPatches(x,y,heading,vision);
+        //add up the total grain in the heading patches that can be seen by the turtle
+        for(int i=0;i<vision;i++){
+            total=total+headingPatches.get(i).getGrainHere();
+            howFar++;
+        }
+        return total;
     }
 
     public void moveEatAgeDie() {
