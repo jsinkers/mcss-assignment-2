9d369f303cefec7ad45b3e689e1cd296681e36a5
James Sinclair <jssinc@student.unimelb.edu.au>
2021-05-26 22:33:32 +1000

    Make inheritance the outer loops in batch script

--- wealth-distrib.bat
+++ wealth-distrib.bat
@@ -8,13 +8,13 @@ javac -d out src/*.java --class-path=src
 set runs=default 2 3 4 5 6 7 8 9 10 11 12
 set inheritance=true false
 
-:: for each run
-echo test
-for %%r in (%runs%) do (
-    echo %%r
-    :: for each random seed
-    for /l %%s in (0, 1, 100) do (
-        for %%i in (%inheritance%) do (
+:: with/without inheritance
+for %%i in (%inheritance%) do (
+    :: for each run
+    for %%r in (%runs%) do (
+        echo %%r
+        :: for each random seed
+        for /l %%s in (0, 1, 100) do (
             java -classpath out World props/wealth-distrib-%%r.properties %%s %%i
         )
     )
