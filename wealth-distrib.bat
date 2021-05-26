9d2d6d8cb69fefb119f07cc3e5bd46b79833a8e5
James Sinclair <jssinc@student.unimelb.edu.au>
2021-05-26 22:30:35 +1000

    Update batch script to run inheritance extension

--- wealth-distrib.bat
+++ wealth-distrib.bat
@@ -6,7 +6,7 @@ javac -d out src/*.java --class-path=src
 
 :: list of runs to use
 set runs=default 2 3 4 5 6 7 8 9 10 11 12
-::set runs=default
+set inheritance=true false
 
 :: for each run
 echo test
@@ -14,6 +14,8 @@ for %%r in (%runs%) do (
     echo %%r
     :: for each random seed
     for /l %%s in (0, 1, 100) do (
-        java -classpath out World props/wealth-distrib-%%r.properties %%s
+        for %%i in (%inheritance%) do (
+            java -classpath out World props/wealth-distrib-%%r.properties %%s %%i
+        )
     )
 )
